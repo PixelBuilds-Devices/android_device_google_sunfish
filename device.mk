eeb15dea7ff16533800733c1ca416992fa793ec9
PixelBoot <bootingpixels@gmail.com>
2023-04-19 16:58:58 +0200

    Revert "Preopt SystemUI for AOSP builds."
    This reverts commit 18ba3e75680f005269f97d89d13598897498067c.

--- device.mk
+++ device.mk
@@ -947,8 +947,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.sys.modem.diag.mdlog_br_num=5
 
 # Preopt SystemUI
-PRODUCT_DEXPREOPT_SPEED_APPS += SystemUIGoogle  # For internal
-PRODUCT_DEXPREOPT_SPEED_APPS += SystemUI  # For AOSP
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    SystemUIGoogle
 
 # Compile SystemUI on device with `speed`.
 PRODUCT_PROPERTY_OVERRIDES += \
