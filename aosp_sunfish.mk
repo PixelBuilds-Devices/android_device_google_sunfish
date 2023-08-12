7b6a42ee1f2a629927f435812010c25cc60767ba
Michael Bestas <mkbestas@lineageos.org>
2023-08-12 12:05:44 +0300

    Update fingerprint/build description from TQ3A.230805.001
    Change-Id: I8e24b4edab222611911c96f4741fbf24cc6599c4

--- aosp_sunfish.mk
+++ aosp_sunfish.mk
@@ -73,8 +73,8 @@ PRODUCT_MODEL := Pixel 4a
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=sunfish \
-    PRIVATE_BUILD_DESC="sunfish-user 13 TQ3A.230705.001 10216780 release-keys"
+    PRIVATE_BUILD_DESC="sunfish-user 13 TQ3A.230805.001 10316531 release-keys"
 
-BUILD_FINGERPRINT := google/sunfish/sunfish:13/TQ3A.230705.001/10216780:user/release-keys
+BUILD_FINGERPRINT := google/sunfish/sunfish:13/TQ3A.230805.001/10316531:user/release-keys
 
 $(call inherit-product, vendor/google/sunfish/sunfish-vendor.mk)
