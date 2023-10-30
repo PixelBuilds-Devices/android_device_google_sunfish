f1c463e79f278166976a423ffdd84bb73f7c92ee
Michael Bestas <mkbestas@lineageos.org>
2023-10-30 22:16:13 +0200

    sunfish: Include missing confirmationui sepolicy
    Change-Id: Ie33e4c6ffb8276b22258ab563c66cb3bbec9472b

--- sepolicy/sunfish-sepolicy.mk
+++ sepolicy/sunfish-sepolicy.mk
@@ -14,4 +14,5 @@ SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += device/google/sunfish/sepolicy/system_ext/pr
 
 # Pixel-wide sepolicy
 BOARD_VENDOR_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/googlebattery
+BOARD_VENDOR_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/confirmationui_hal
 BOARD_VENDOR_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/powerstats
